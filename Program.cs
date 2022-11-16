75e610ab4a6646be8b32c6474473f7bfe3dace33
MiroslavPukovski <miroslav.pukovski@gmail.com>
2022-11-16 17:12:32 +0200

    fixed

--- Program.cs
+++ Program.cs
@@ -1,53 +1,51 @@
-﻿namespace BranchTreeExcercice
+﻿
+internal class Program
 {
-    internal class Program
+    static int depth = 1;
+    static int maxDepth = 1;
+
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var tree = new Branch();
+        tree.branches = new List<Branch> { new Branch(), new Branch() };
+        tree.branches[0].branches = new List<Branch> { new Branch() };
+        tree.branches[1].branches = new List<Branch> { new Branch(), new Branch(), new Branch() };
+        tree.branches[1].branches[0].branches = new List<Branch> { new Branch() };
+        tree.branches[1].branches[1].branches = new List<Branch> { new Branch(), new Branch() };
+        tree.branches[1].branches[1].branches[0].branches = new List<Branch> { new Branch() };
+
+        CalculateTreeDepth(tree);
+        Console.WriteLine(maxDepth);
+    }
+
+    static void CalculateTreeDepth(Branch tree)
+    {
+        if (tree == null)
         {
-            var tree = new Branch();
-            tree.branches = new List<Branch> { new Branch(), new Branch() };
-            tree.branches[0].branches = new List<Branch> { new Branch() };
-            tree.branches[1].branches = new List<Branch> { new Branch(), new Branch(), new Branch() };
-            tree.branches[1].branches[0].branches = new List<Branch> { new Branch() };
-            tree.branches[1].branches[1].branches = new List<Branch> { new Branch(), new Branch() };
-            tree.branches[1].branches[1].branches[0].branches = new List<Branch> { new Branch() };
+            maxDepth--;
+            return;
+        }
 
-            var depth = 1;
-            var maxDepth = 1;
-
-            CalculateTreeDepth(tree);
-            Console.WriteLine(maxDepth);
-
-
-            void CalculateTreeDepth(Branch tree)
+        if (tree.branches != null)
+        {
+            depth++;
+            foreach (var branch in tree.branches)
+                CalculateTreeDepth(branch);
+        }
+        else
+        {
+            if (depth > maxDepth)
             {
-                if (tree == null)
-                {
-                    maxDepth--;
-                    return;
-                }
-
-                if (tree.branches != null)
-                {
-                    depth++;
-                    foreach (var branch in tree.branches)
-                        CalculateTreeDepth(branch);
-                }
-                else
-                {
-                    if (depth > maxDepth)
-                    {
-                        maxDepth = depth;
-                        depth--;
-                    }
-                }
+                maxDepth = depth;
+                depth--;
             }
         }
-
-        public class Branch
-        {
-            public List<Branch>? branches;
-        }
     }
-    
 }
+    
+public class Branch
+{
+    public List<Branch>? branches;
+}
+    
+    
